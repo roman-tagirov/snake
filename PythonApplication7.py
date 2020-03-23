d0955f3058db7ba3dbd043c1a742569d2661d994
Hank Magreger <delayemuroki@mail.ru>
2020-03-23 22:05:03 +0500

    Змея ползает

--- PythonApplication7.py
+++ PythonApplication7.py
@@ -1,8 +1,39 @@
-class segment:
-    def __init__(self,x,y):
+from tkinter import Tk,Canvas
+WIDTH=800
+HEIGHT=600
+from tkinter import Tk,Canvas
+WIDTH=800
+HEIGHT=600
+SIZE = 15
+class Segment(object):
+    def __init__(self,x,y,c): 
         self.x = x
         self.y = y
-class snake:
-    segments:[segment(4,1),segment(5,1),segment(6,1)]
-    def moveright(self):
-        head = segment[0]
+        self.c = c;
+        self.shape = c.create_rectangle(self.x*SIZE,self.y*SIZE,(self.x + 1)*SIZE,(self.y+1)*SIZE,fill = "blue")
+
+    def move(self,x, y):
+        self.c.move(self.shape, (x- self.x)*SIZE, (y- self.y)*SIZE)
+        self.x=x
+        self.y=y
+class Snake:
+    def __init__(self,c):
+        self.c = c 
+        self.segments = [Segment(1,3,c),Segment(1,2,c),Segment(1,1,c)]
+    def move(self):
+        #for index in range(len(self.segments)-1):
+        #  s = self.segments[index]
+        #  self.segments[index+1].x = s.x
+        #  self.segments[index+1].y = s.y
+        self.segments[1].move(self.segments[1].x,self.segments[1].y+1)
+def main():
+    snake.move()
+    root.after(100, main)
+root = Tk()
+root.title("Змейка")
+c = Canvas(root, width=WIDTH,height=HEIGHT, bg = "green")
+c.grid()
+c.focus_set()
+snake = Snake(c)
+main()
+root.mainloop()
